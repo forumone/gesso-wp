e12cf5ef0f4fad800d389b7c38f9920651db3314
Lindsey Di Napoli <lalindsey@cssgirl.com>
2014-10-16 11:17:06 -0400

    added footer widget region

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 	  <footer class="footer" role="contentinfo">
 	  	<div class="footer-inner layout-constrain">
+	  	
+	  		<?php get_sidebar('footer'); ?>
+	  	
 				<p class="copyright">
 					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. 				
 				</p>
