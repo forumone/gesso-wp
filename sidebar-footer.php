9b9944b6e8ea012403f4c6280ac9edb7c915d589
Lindsey Di Napoli <lalindsey@cssgirl.com>
2015-03-04 17:23:24 -0500

    added accessible aria tag to footer widget sidebar area

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,1 +1,5 @@
-<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widgets')) ?>
+<aside class="footer-sidebar" role="complementary">
+	<div class="footer-sidebar__widgets">
+		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widgets')) ?>
+	</div>
+</aside>
