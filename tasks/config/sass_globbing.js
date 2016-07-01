f37a8df64bf06966e6587d86aaa2be3e212cbbb2
Derek Moon <dmoon@dmoon-mac-2.local>
2016-07-01 11:09:50 -0700

    fix misplaced quote

--- tasks/config/sass_globbing.js
+++ tasks/config/sass_globbing.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     sass_globbing : {
       gesso : {
         files : (function(grunt) {
-          var opts = grunt.file.readJSON('grunt.config.get('pkg').themePath + sass/sass-globbing.json');
+          var opts = grunt.file.readJSON(grunt.config.get('pkg').themePath + 'sass/sass-globbing.json');
           return Object.keys(opts).reduce(function(previous, current) {
             previous[grunt.config.get('pkg').themePath + 'sass/' + current] = grunt.config.get('pkg').themePath + 'sass/' + opts[current];
             return previous;
