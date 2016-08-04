9b0ab77dcd5bac7353b16998f1f5594ac590c4e8
Derek Moon <dmoon@dmoon-mac-2.local>
2016-08-04 12:36:47 -0700

    update 404 page to timber

--- 404.php
+++ 404.php
@@ -1,25 +1,5 @@
-<?php get_header(); ?>
-
-	<main id="main" class="main" role="main" tabindex="-1">	
-    <?php if (has_visible_widgets('widget-area-1')) { $sidebarclasses = 'sidebar'; } else { $sidebarclasses = 'no-sidebar'; }?>
-		<div class="layout-main layout-constrain <?php echo $sidebarclasses; ?>">
-			
-			<div class="layout-main__content">
-				<section>
-					<article id="post-404">
-						<h1 class="page-title"><?php _e( 'Uh oh. Page not found.', 'gesso' ); ?></h1>
-						<h2>
-							<a href="<?php echo home_url(); ?>"><?php _e( 'Return to homepage?', 'gesso' ); ?></a>
-						</h2>
-					</article>
-				</section>
-			</div>
-
-			<div class="layout-main__sidebar">
-				<?php get_sidebar(); ?>
-			</div>
-		
-		</div>
-	</main>
-
-<?php get_footer(); ?>
+<?php
+$data = Timber::get_context();
+$context['post'] = $post;
+Timber::render( '404.twig', $context );
+?>
