ff52a6ef06309e5a6491b5d8bad99b8f1ed18744
Derek Moon <dmoon@forumone.com>
2016-11-08 17:29:33 -0800

    working on search

---
+++ _search.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+	<main id="main" class="main" role="main" tabindex="-1">	
+    <?php if (has_visible_widgets('widget-area-1')) { $sidebarclasses = 'sidebar'; } else { $sidebarclasses = 'no-sidebar'; }?>
+		<div class="layout-main layout-constrain <?php echo $sidebarclasses; ?>">
+			
+			<div class="layout-main__content">
+				<section>
+					<h1 class="page-title"><?php echo sprintf( __( '%s Search Results for ', 'gesso' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+					<?php get_template_part('templates/loop'); ?>
+					<?php get_template_part('templates/pagination'); ?>
+				</section>
+			</div>
+
+			<div class="layout-main__sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+		
+		</div>
+	</main>
+
+<?php get_footer(); ?>
--- author.php
+++ author.php
@@ -17,4 +17,4 @@ if ( isset( $wp_query->query_vars['author'] ) ) {
 	$context['author'] = $author;
 	$context['title'] = 'Author Archives: ' . $author->name();
 }
-Timber::render( array( 'author.twig', 'archive.twig' ), $context );
+Timber::render( array( 'author.twig', 'archive.twig', 'search.twig' ), $context );
