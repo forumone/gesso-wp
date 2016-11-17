4c53893afb474f8434b6b6714918e4119a73ca4e
Derek Moon <dmoon@forumone.com>
2016-11-17 15:56:34 -0800

    configure search.php page

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
     $context = Timber::get_context();
     $context['pagination'] = Timber::get_pagination();
-    Timber::render( 'search-results.twig', $context );
+    Timber::render( 'search.twig', $context );
 ?>
