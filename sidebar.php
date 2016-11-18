92442fe5911de5164189da333fe40a706be1be42
Derek Moon <dmoon@forumone.com>
2016-11-17 16:30:12 -0800

    sidebar & footer cleanup

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,6 @@
 <aside class="sidebar" role="complementary">
 	<div class="sidebar__widgets">
+    <h1>SIDEBAR </h1>
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
 	</div>
 </aside>
