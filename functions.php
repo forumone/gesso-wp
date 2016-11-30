b01a13d0d16c42b645a8f8c1448da0bb8d89ab77
Derek Moon <dmoon@forumone.com>
2016-11-29 16:33:19 -0800

    add comments, beautify code

--- functions.php
+++ functions.php
@@ -308,6 +308,7 @@ class StarterSite extends TimberSite {
     $context['stuff'] = 'I am a value set in your functions.php file';
     $context['notes'] = 'These values are available everytime you call Timber::get_context();';
     $context['menu'] = new TimberMenu();
+    $context['current_year'] = date('Y');
     $context['site'] = $this;
     return $context;
   }
