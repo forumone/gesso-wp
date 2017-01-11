85a7055fb62fd0a5f54925045cbdc04bb1838716
Elvis Morales <emorales@forumone.com>
2017-01-11 03:32:44 -0800

    Removing placeholders to register new CPT and custom Taxonomies, this should be declared on plugin territory, not in the template functions.php

--- functions.php
+++ functions.php
@@ -290,17 +290,7 @@ class StarterSite extends TimberSite {
     add_theme_support( 'menus' );
     add_filter( 'timber_context', array( $this, 'add_to_context' ) );
     add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-    add_action( 'init', array( $this, 'register_post_types' ) );
-    add_action( 'init', array( $this, 'register_taxonomies' ) );
     parent::__construct();
-  }
-
-  function register_post_types() {
-    //this is where you can register custom post types
-  }
-
-  function register_taxonomies() {
-    //this is where you can register custom taxonomies
   }
 
   function add_to_context( $context ) {
