46cad507818eb22708907a6f7e4f5cc746eb93b8
Elvis Morales <emorales@forumone.com>
2017-01-11 02:47:25 -0800

    Enable large and medium image sizes.

--- functions.php
+++ functions.php
@@ -16,9 +16,9 @@ if ( function_exists('add_theme_support') ) {
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
 
-  # Define automatic thumbnail sizes
-  // add_image_size( 'large', 700, '', true ); // Large Thumbnail
-	// add_image_size( 'medium', 250, '', true ); // Medium Thumbnail
+  // Define automatic thumbnail sizes
+  	add_image_size( 'large', 700, '', true ); // Large Thumbnail
+	add_image_size( 'medium', 250, '', true ); // Medium Thumbnail
 	add_image_size( 'small', 120, '', true ); // Small Thumbnail
 	// add_image_size( 'custom-size', 700, 200, true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 }
