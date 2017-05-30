a0c28f972b07b1db291460000aa3b6871801fb53
Elvis Morales <emorales@forumone.com>
2017-05-30 15:58:52 -0700

    Housekeeping removing non relevant comment for this template.

--- page.php
+++ page.php
@@ -14,7 +14,6 @@
  * /mytheme/page-mypage.php
  * (in which case you'll want to duplicate this file and save to the above path)
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
  *
  * @package  WordPress
  * @subpackage  Timber
--- single.php
+++ single.php
@@ -2,8 +2,6 @@
 /**
  * The Template for displaying all single posts
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
  * @package  WordPress
  * @subpackage  Timber
  * @since    Timber 0.1
