6842ecba541538e19de7eb30f06303d0c85e1efc
Kelli Monahan <kmonahan@forumone.com>
2019-09-17 14:56:47 -0500

    Refactor SCSS to go along with updated JS

--- js/src/modules/MobileMenu.js
+++ js/src/modules/MobileMenu.js
@@ -11,12 +11,12 @@ const MobileMenu = function() {
     header: '.l-header', // Selector for site header
     toggleButton: '.mobile-menu__button', //Selector for Menu toggle
     container: '.mobile-menu-container', // Selector Destination container for mobile nav
-    mobileNavClass: 'mobile-menu-nav', // Classname for nav wrapping the mobile menu
-    mobileMenuClass: 'mobile-menu', // Classname for navigation section
-    mobileSearchClass: 'mobile-search-block', //Classname for search section
-    mobileUtilityMenuClass: 'mobile-account-menu', //Classname for utility section
-    closeButtonClass: 'mobile-menu__close', //Classname for generated close button
-    overlayClass: 'mobile-menu__overlay', //Overlay Classname
+    mobileMenuClass: 'mobile-nav', // Classname for navigation section
+    mobileSearchClass: 'mobile-search-block', // Classname for search section
+    mobileUtilityMenuClass: 'mobile-account-menu', // Classname for utility section
+    buttonClass: 'mobile-menu__button',
+    closeButtonClass: 'mobile-menu__close', // Classname for generated close button
+    overlayClass: 'mobile-menu', // Overlay Classname
   };
 
   let currOptions = {};
@@ -41,26 +41,18 @@ const MobileMenu = function() {
       const header = document.querySelector(currOptions.header);
       const closeButton = document.createElement('button');
       let toggleButton = document.querySelector(currOptions.toggleButton);
-      overlay = document.createElement('div');
+      overlay = document.createElement('nav');
       container = document.querySelector(currOptions.container);
 
-      if (container === null) {
-        container = document.createElement('nav');
-        container.classList.add(currOptions.container.substr(1));
-        if (header) {
-          header.insertAdjacentElement('afterend', container);
-        } else {
-          document.body.insertAdjacentElement('afterbegin', container);
-        }
-      }
-
+      // Multiple calls to classList.add() here because IE doesn't support multiple arguments. :(
+      closeButton.classList.add(currOptions.buttonClass);
       closeButton.classList.add(currOptions.closeButtonClass);
       closeButton.innerHTML = 'X';
       closeButton.addEventListener('click', closeMenu);
 
       if (toggleButton === null) {
         toggleButton = document.createElement('button');
-        toggleButton.classList.add(currOptions.toggleButton.substr(1));
+        toggleButton.classList.add(currOptions.buttonClass);
         toggleButton.innerHTML =
           '<span class="mobile-menu__icon mobile-menu__icon--menu">Menu</span>';
         header.insertAdjacentElement('beforeend', toggleButton);
@@ -70,16 +62,23 @@ const MobileMenu = function() {
       });
 
       overlay.classList.add(currOptions.overlayClass);
-      container.appendChild(closeButton);
+      overlay.appendChild(closeButton);
 
       if (searchBlock) {
-        container.appendChild(cloneMenu(searchBlock, 'search'));
+        overlay.appendChild(cloneMenu(searchBlock, 'search'));
       }
-      container.appendChild(cloneMenu(navMenu));
+      overlay.appendChild(cloneMenu(navMenu));
       if (utilityMenu) {
-        container.appendChild(cloneMenu(utilityMenu, 'account'));
+        overlay.appendChild(cloneMenu(utilityMenu, 'account'));
       }
-      container.appendChild(overlay);
+
+      if (container !== null) {
+        container.appendChild(overlay);
+      } else if (header !== null) {
+        header.insertAdjacentElement('afterend', overlay);
+      } else {
+        document.body.insertAdjacentElement('afterbegin', container);
+      }
       closeMenu();
 
       document.body.classList.add('mobile-menu-processed');
@@ -124,17 +123,16 @@ const MobileMenu = function() {
   }
 
   function closeMenu() {
-    const menu = container.querySelector('ul');
+    const menu = overlay.querySelector('ul');
     const links = [...menu.querySelectorAll('.menu__link')];
 
     setTabIndex(links, -1);
 
     //Remove Event Listeners
-    overlay.removeEventListener('click', closeMenu);
     document.removeEventListener('keydown', handleKeyDown);
 
     const btns = [
-      ...container.getElementsByClassName(currOptions.closeButtonClass),
+      ...overlay.getElementsByClassName(currOptions.closeButtonClass),
     ];
 
     btns.forEach(btn => {
@@ -142,38 +140,37 @@ const MobileMenu = function() {
       btn.removeEventListener('click', closeMenu);
     });
 
-    container.classList.remove('is-open');
-    container.setAttribute('style', 'display: none;');
+    overlay.classList.remove('is-open');
+    overlay.setAttribute('style', 'display: none;');
 
     prevFocused.focus();
   }
 
   function openMenu() {
-    const menu = container.querySelector('ul');
+    const menu = overlay.querySelector('ul');
     const links = [...menu.querySelectorAll('.menu__link')];
 
     setTabIndex(links, 0);
     // ....
     prevFocused = document.activeElement;
 
-    container.classList.add('is-open');
-    overlay.addEventListener('click', closeMenu);
+    overlay.classList.add('is-open');
 
     const btns = [
-      ...container.getElementsByClassName(currOptions.closeButtonClass),
+      ...overlay.getElementsByClassName(currOptions.closeButtonClass),
     ];
     btns.forEach(btn => {
       setTabIndex(btn, 0);
       btn.addEventListener('click', closeMenu);
     });
-    container.setAttribute('style', 'display: block;');
+    overlay.setAttribute('style', 'display: block;');
 
     document.addEventListener('keydown', handleKeyDown);
   }
 
   function handleKeyDown(e) {
     // Select all focusable items
-    const focusable = container.querySelectorAll(
+    const focusable = overlay.querySelectorAll(
       'button, [href], input, select, textarea,[tabindex]:not([tabindex="-1"]'
     );
 
