f8e6ab1d5fda9fa57f2789b3f11c00434f912db7
Kelli Monahan <kmonahan@forumone.com>
2019-09-17 10:03:36 -0500

    Load global scripts in footer, not header

--- functions.php
+++ functions.php
@@ -59,7 +59,7 @@ function gesso_scripts() {
     wp_enqueue_script( "comment-reply" );
   }
 
-  wp_register_script('gessoscripts', get_template_directory_uri() . '/js/dist/scripts.min.js', array('gessomodernizr') ); // Custom scripts
+  wp_register_script('gessoscripts', get_template_directory_uri() . '/js/dist/scripts.min.js', array('gessomodernizr'), false, true ); // Custom scripts
   wp_enqueue_script('gessoscripts');
 
   wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/css/styles.css', array(), null, 'all' );
