b4aa83654679a0a18f2231ce0f93b69cc82195e8
coreylafferty <coreylafferty@gmail.com>
2021-02-15 11:45:11 -0800

    fixes wsod from timber upgrade by properly using get_footer (#227)
    * fixes wsod from timber upgrade by properly using get_footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Third party plugins that hijack the theme will call wp_footer() to get the footer template.
+ * Third party plugins that hijack the theme will call get_footer() to get the footer template.
  * We use this to end our output buffer (started in header.php) and render into the view/page-plugin.twig template.
  */
 $timberContext = $GLOBALS['timberContext'];
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Third party plugins that hijack the theme will call wp_head() to get the header template.
+ * Third party plugins that hijack the theme will call get_header() to get the header template.
  * We use this to start our output buffer and render into the templates/page-plugin.twig template in footer.php
  */
 $GLOBALS['timberContext'] = Timber::get_context();
