0f7318aab7cf36071a6d061c2e3faf1f0a3967a8
Tim Nolte <tnolte@forumone.com>
2021-08-05 13:31:06 -0400

    Fixes Google Fonts Preconnect Setup & Coding Standards

--- functions.php
+++ functions.php
@@ -114,20 +114,45 @@ function gesso_scripts() {
 	wp_enqueue_script( 'gessoscripts' );
 
 	// Enqueue Google Fonts.
-	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap', array(), null );
+	/**
+	 * Google font preconnect setup doesn't work when version is specified.
+	 *
+	 * phpcs:disable WordPress.WP.EnqueuedResourceParameters.MissingVersion
+	 */
+	wp_enqueue_style( 'google-fonts-preconnect-api', 'https://fonts.googleapis.com', array(), null );
+	wp_enqueue_style( 'google-fonts-preconnect', 'https://fonts.gstatic.com', array( 'google-fonts-preconnect-api' ), null );
+	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap', array( 'google-fonts-preconnect' ), null );
+	// phpcs:enable
 
 	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/css/styles.css', array(), filemtime( get_stylesheet_directory() . '/css/styles.css' ), 'all' );
 
 }
-/**
- * Setup Google preconnect links.
- */
-function google_preconnect() {
-	echo '<link rel="preconnect" href="https://fonts.googleapis.com">
-	  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
-}
-add_action( 'wp_head', 'google_preconnect', 0 );
 add_action( 'wp_enqueue_scripts', 'gesso_scripts' );
+
+/**
+ * Filter enqueue styles.
+ *
+ * @param string $tag    The link tag for the enqueued style.
+ * @param string $handle The style's registered handle.
+ * @param string $href   The stylesheet's source URL.
+ * @param string $media  The stylesheet's media attribute.
+ *
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter) $media does not need to be checked or used in this case.
+ *
+ * @return string
+ */
+function gesso_google_font_enqueued_styles( $tag, $handle, $href, $media ) {
+	$handles = array( 'google-fonts-preconnect', 'google-fonts-preconnect-api' );
+	// Google font preconnect rewrite.
+	if ( in_array( $handle, $handles ) ) {
+		$tag = '<link rel="preconnect" href="' . $href . '" />';
+		if ( boolval( strpos( $href, 'gstatic' ) ) ) {
+			$tag = '<link rel="preconnect" href="' . $href . '" crossorigin />';
+		}
+	}
+	return $tag;
+}
+add_filter( 'style_loader_tag', 'gesso_google_font_enqueued_styles', 10, 4 );
 
 /**
  * Setup admin specific scripts.
