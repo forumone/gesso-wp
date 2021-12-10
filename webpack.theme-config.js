48d44e558415bcfa2da40a47d2bf516f420581c2
KJ Monahan <kj@forumone.com>
2021-12-10 09:56:45 -0600

    Port over functionality to handle design tokens

---
+++ webpack.theme-config.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: {
+    'design-tokens': './source/00-config/config.design-tokens.yml',
+    'theme.json': './theme-settings.json',
+  },
+});
