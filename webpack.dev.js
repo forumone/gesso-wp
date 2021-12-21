fd1a9480b16b5c37dbbcec0b7d97fd34192c7a44
KJ Monahan <kj@forumone.com>
2021-12-21 08:14:30 -0500

    Add JS linting and switch to WP coding standards

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'source-map'
+	mode: 'development',
+	devtool: 'source-map',
+	plugins: [
+		new ESLintPlugin({
+			overrideConfigFile: path.resolve(__dirname, '.eslintrc-dev.js'),
+			useEslintrc: false,
+		}),
+	],
 });
